f206012a85400f1a5a6059a0696cd09d731edb58
zukhra1 <149882826+zukhra1@users.noreply.github.com>
2024-04-16 14:49:21 +0300

    stage1.sh

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,1 +1,17 @@
-#!/bin/bash
+#downloading the dataset through powershell(windows)
+$baseUrl = "https://data.sfgov.org/resource/wr8u-xric.csv"
+$outputFolder = "./data"
+$offset = 0
+$limit = 1000
+
+if (-not (Test-Path -Path $outputFolder)) {
+    New-Item -ItemType Directory -Path $outputFolder
+}
+
+do {
+    $url = "$baseUrl`?`$limit=$limit&`$offset=$offset"
+    $fileName = "data_$offset.csv"
+    $filePath = Join-Path -Path $outputFolder -ChildPath $fileName
+    Invoke-WebRequest -Uri $url -OutFile $filePath
+    $offset += $limit
+} while ($limit -eq 1000)
